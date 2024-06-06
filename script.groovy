a07a3c05ed7b25d87889c7d12e160ada18b48b04
bondgh0954 <otengdevops@gmail.com>
2024-06-06 11:51:55 +0200

    modify groovy script

--- script.groovy
+++ script.groovy
@@ -10,9 +10,9 @@ def buildImage(){
     echo "building docker image"
     echo "pushing image to docker repository"
     withCredentials([usernamePassword(credentialsID: "dockerhub-credentials", passwordVariable: "PASSWORD", usernameVariable: "USERNAME")])
-    sh "docker build -t nanaot/maven-app:2.1"
+    sh "docker build -t nanaot/java-maven:2.1"
     sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-    sh "docker push nanaot/maven-app:2.1"
+    sh "docker push nanaot/java-maven:2.1"
 
 
 }
